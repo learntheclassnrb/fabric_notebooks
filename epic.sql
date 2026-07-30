1743df596b1f336bb6b15a7fba48a741cca3c9ee
learntheclassnrb <105121917+learntheclassnrb@users.noreply.github.com>
2026-07-30 11:30:50 -0400

    Update epic.sql56

--- epic.sql
+++ epic.sql
@@ -130,6 +130,158 @@ REPLACE(
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+REPLACE(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(
+                        REPLACE(
+                            REPLACE(
+                                REPLACE(
+                                    REPLACE(
+                                        REPLACE(
+                                            REPLACE(
+                                                REPLACE(
+                                                    REPLACE(
+                                                        REPLACE(
+                                                            REPLACE(
+                                                                REPLACE(
+                                                                    REPLACE(
+                                                                        REPLACE(
+                                                                            REPLACE(
+                                                                                REPLACE(
+                                                                                    IFNULL([DISCHARGE_NOTE_TEXT], ""),
+                                                                                    "DISCHARGE SUMMARY",
+                                                                                    CHAR(10) + CHAR(10) +
+                                                                                    "DISCHARGE SUMMARY" +
+                                                                                    CHAR(10)
+                                                                                ),
+                                                                                "DIAGNOSES:",
+                                                                                CHAR(10) + CHAR(10) +
+                                                                                "DIAGNOSES" +
+                                                                                CHAR(10)
+                                                                            ),
+                                                                            "Diagnoses:",
+                                                                            CHAR(10) + CHAR(10) +
+                                                                            "DIAGNOSES" +
+                                                                            CHAR(10)
+                                                                        ),
+                                                                        "Principal Problem:",
+                                                                        CHAR(10) +
+                                                                        "PRINCIPAL PROBLEM:" +
+                                                                        CHAR(10)
+                                                                    ),
+                                                                    "Active Problems:",
+                                                                    CHAR(10) +
+                                                                    "ACTIVE PROBLEMS:" +
+                                                                    CHAR(10)
+                                                                ),
+                                                                "PROCEDURES:",
+                                                                CHAR(10) + CHAR(10) +
+                                                                "PROCEDURES" +
+                                                                CHAR(10)
+                                                            ),
+                                                            "CLINICAL INDICATION:",
+                                                            CHAR(10) + CHAR(10) +
+                                                            "CLINICAL INDICATION" +
+                                                            CHAR(10)
+                                                        ),
+                                                        "FINDINGS:",
+                                                        CHAR(10) + CHAR(10) +
+                                                        "FINDINGS" +
+                                                        CHAR(10)
+                                                    ),
+                                                    "IMPRESSION:",
+                                                    CHAR(10) + CHAR(10) +
+                                                    "IMPRESSION" +
+                                                    CHAR(10)
+                                                ),
+                                                "REASON FOR HOSPITALIZATION",
+                                                CHAR(10) + CHAR(10) +
+                                                "REASON FOR HOSPITALIZATION" +
+                                                CHAR(10)
+                                            ),
+                                            "HOSPITAL COURSE",
+                                            CHAR(10) + CHAR(10) +
+                                            "HOSPITAL COURSE" +
+                                            CHAR(10)
+                                        ),
+                                        "PHYSICAL EXAM",
+                                        CHAR(10) + CHAR(10) +
+                                        "PHYSICAL EXAM" +
+                                        CHAR(10)
+                                    ),
+                                    "DISPOSITION / INSTRUCTIONS",
+                                    CHAR(10) + CHAR(10) +
+                                    "DISPOSITION / INSTRUCTIONS" +
+                                    CHAR(10)
+                                ),
+                                "DISPOSITION/INSTRUCTIONS",
+                                CHAR(10) + CHAR(10) +
+                                "DISPOSITION / INSTRUCTIONS" +
+                                CHAR(10)
+                            ),
+                            "Disposition:",
+                            CHAR(10) +
+                            "DISPOSITION:" +
+                            CHAR(10)
+                        ),
+                        "Medication Orders (From admission, onward)",
+                        CHAR(10) + CHAR(10) +
+                        "MEDICATION ORDERS" +
+                        CHAR(10)
+                    ),
+                    "Discharge Orders/Instructions",
+                    CHAR(10) + CHAR(10) +
+                    "DISCHARGE ORDERS / INSTRUCTIONS" +
+                    CHAR(10)
+                ),
+                "DISCHARGE MEDICATIONS",
+                CHAR(10) + CHAR(10) +
+                "DISCHARGE MEDICATIONS" +
+                CHAR(10)
+            ),
+            "Discharge Medications:",
+            CHAR(10) + CHAR(10) +
+            "DISCHARGE MEDICATIONS" +
+            CHAR(10)
+        ),
+        "PCP/PROVIDER FOLLOW-UP ITEMS",
+        CHAR(10) + CHAR(10) +
+        "PCP / PROVIDER FOLLOW-UP ITEMS" +
+        CHAR(10)
+    ),
+    "Follow-up:",
+    CHAR(10) + CHAR(10) +
+    "FOLLOW-UP" +
+    CHAR(10)
+)
+
+
+
+
+
 REPLACE(
     REPLACE(
         REPLACE(
